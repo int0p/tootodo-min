b615f213fe52527bb00e1ded34ea6600f5b2ea2f
seungyeah <closer.fz@gmail.com>
2023-08-27 07:55:53 +0900

    gantt start

--- src/routes/(schedule)/too/timeline/+page.svelte
+++ src/routes/(schedule)/too/timeline/+page.svelte
@@ -19,34 +19,31 @@
 </script>
 
 
-<table class="w-full bg-white h-[50px] ">
-    <thead class="border-2">
+<table class="w-full bg-white h-[50px] rounded-t-xl border-b text-black">
+    <thead class="">
     <tr class="sticky">
-        <th rowspan="2" class="border w-[4%]"><button class="">v</button></th>
-        <th rowspan="2" class="border w-[20%]">milestone</th>
-        <th rowspan="2" class="border w-[7%]">status</th>
-        <th rowspan="2" class="border w-[13%]">duration</th>
-        <th class="w-[56%]"><input type="date"/>
-            -
-            <input type="date"/></th>
+        <th rowspan="2" class="border-r w-[4%]"><button class="">v</button></th>
+        <th rowspan="2" class="border-r w-[20%]">milestone</th>
+        <th rowspan="2" class="border-r w-[7%]">status</th>
+        <th rowspan="2" class="border-r w-[13%]">duration</th>
+        <th class="border-b w-[56%]"><input type="date"/>            -            <input type="date"/></th>
     </tr>
-    <tr class="border">
+    <tr class="">
         <td>            start        </td>
     </tr>
     </thead>
 </table>
 
-<div class="w-[calc(100%+8px)] h-full max-h-[calc(100%-70px)] overflow-y-auto">
+<div class="w-[calc(100%+8px)] h-[calc(100%-70px)] overflow-y-scroll ">
     {#each Array(3) as _,i}
-    <div class="card border-t-2  rounded-t-xl my-2">
-        <header class="header h-[40px] flex justify-start items-center space-x-1
-            bg-white/30 dark:bg-black/40 relative">
-            <span class="font-bold text-xl px-4">Project {i}</span>
-            <button class="btn absolute right-0">
-                add project
+    <div class="my-2 border-b-2">
+        <header class="h-[40px] bg-white/40">
+            <button class="btn">
+                +
             </button>
+            <span class="font-bold text-xl ">Project {i}</span>
         </header>
-        <table class="w-full bg-white">
+        <table class="w-full bg-white text-black">
             <thead class="">
             <tr >
                 <th class="w-[4%]"></th>
@@ -56,18 +53,18 @@
                 <th class="w-[56%]"></th>
             </tr>
             </thead>
-            <tbody class="border-2">
+            <tbody class="">
             {#each milestones as milestone,i}
-                <tr class="border-2">
-                    <td class="border text-center">v</td>
-                    <td class="border pl-2">milestone-name {i}</td>
-                    <td class="border pl-2">state {i}</td>
-                    <td class="border">
+                <tr class="border-t-2">
+                    <td class="border-r text-center">+</td>
+                    <td class="border-r pl-2">milestone-name {i}</td>
+                    <td class="border-r pl-2">state {i}</td>
+                    <td class="border-r">
                         <span>{milestone.start.getMonth()+1}월 {milestone.start.getDate()}일</span>
                         <span>-</span>
                         <span>{milestone.end.getMonth()+1}월 {milestone.end.getDate()}일</span>
                     </td>
-                    <td><div class="">
+                    <td><div class="border-b">
                         <div id="milestone"
                              style="margin-left: {daysBetween(milestones[0].start, milestone.start) * 20}px;
                             width: {daysBetween(milestone.start, milestone.end) * 20}px;">
